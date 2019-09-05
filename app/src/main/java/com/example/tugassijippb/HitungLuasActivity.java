522fbf57c51b7cb3e0c3074ef98d7e1038b77168
Faizal Khilmi Muzakki <faizal.muzakki@gmail.com>
2019-09-05 22:46:45 +0700

    case edittext kosong

--- app/src/main/java/com/example/tugassijippb/HitungLuasActivity.java
+++ app/src/main/java/com/example/tugassijippb/HitungLuasActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class HitungLuasActivity extends AppCompatActivity {
 
@@ -17,6 +18,7 @@ public class HitungLuasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Ngitung Luas Segi 4");
 
         btn_hitung = findViewById(R.id.btn_hitung);
         panjang = findViewById(R.id.ed_panjang);
@@ -26,6 +28,16 @@ public class HitungLuasActivity extends AppCompatActivity {
         btn_hitung.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (panjang.getText().toString().matches("")) {
+                    Toast.makeText(v.getContext(), "Sampeyan rung nginput panjange", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (lebar.getText().toString().matches("")) {
+                    Toast.makeText(v.getContext(), "Sampeyan rung nginput panjange", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 double panjang_num = Double.parseDouble(panjang.getText().toString().trim());
                 double lebar_num = Double.parseDouble(lebar.getText().toString().trim());
                 double hasil_num = panjang_num * lebar_num;
